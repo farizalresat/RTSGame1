b2b25db5725e7ae07a1e0be1794d3666f9b3a821
farizalresat <farizalresat@gmail.com>
2020-01-04 23:21:44 +0800

    fourth January commit

---
+++ Script/Camera2D.gd
@@ -0,0 +1,23 @@
+extends Camera2D
+
+export var speed = 10.0
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	var inpx = (int(Input.is_action_pressed("ui_right")) 
+            - int(Input.is_action_just_pressed("ui_left")))
+	var inpy = (int(Input.is_action_pressed("ui_down")) 
+            - int(Input.is_action_just_pressed("ui_up")))
+	position.x = lerp(position.x, position.x + inpx, speed)
+	position.y = lerp(position.y, position.y + inpy, speed)
+	
+	pass
+
